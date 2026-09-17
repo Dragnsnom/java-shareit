ae5c8ee69d7061b9c0b84e5b6edae2a830935451
u_m23qk <VButesku@alfabank.ru>
2026-09-18 00:32:21 +0300

    fix jacoco

---
+++ gateway/src/test/java/ru/practicum/shareit/item/ItemClientTest.java
@@ -0,0 +1,111 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.client.MockRestServiceServer;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+class ItemClientTest {
+
+    private ItemClient client;
+    private MockRestServiceServer server;
+
+    @BeforeEach
+    void setUp() {
+        client = new ItemClient("http://localhost:9090", new RestTemplateBuilder());
+        server = MockRestServiceServer.bindTo(client.getRestTemplate()).build();
+    }
+
+    @Test
+    void createItem_SendsPostWithUserHeader() {
+        server.expect(requestTo("http://localhost:9090/items"))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(header("X-Sharer-User-Id", "1"))
+                .andRespond(withSuccess("{\"id\":1}", MediaType.APPLICATION_JSON));
+
+        ItemDto dto = new ItemDto();
+        dto.setName("Drill");
+        dto.setDescription("Powerful drill");
+        dto.setAvailable(true);
+        ResponseEntity<Object> response = client.createItem(1L, dto);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void updateItem_SendsPatchToItemsIdEndpoint() {
+        server.expect(requestTo("http://localhost:9090/items/10"))
+                .andExpect(method(HttpMethod.PATCH))
+                .andRespond(withSuccess("{\"id\":10}", MediaType.APPLICATION_JSON));
+
+        ItemDto dto = new ItemDto();
+        dto.setName("New name");
+        ResponseEntity<Object> response = client.updateItem(1L, 10L, dto);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void getItemById_SendsGetToItemsIdEndpoint() {
+        server.expect(requestTo("http://localhost:9090/items/10"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("{\"id\":10}", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.getItemById(1L, 10L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void getItemsByOwnerId_SendsGetToItemsEndpoint() {
+        server.expect(requestTo("http://localhost:9090/items"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.getItemsByOwnerId(1L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void searchItems_SendsGetWithTextQueryParam() {
+        server.expect(requestTo("http://localhost:9090/items/search?text=drill"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.searchItems("drill");
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void addComment_SendsPostToCommentEndpoint() {
+        server.expect(requestTo("http://localhost:9090/items/10/comment"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess("{\"id\":1}", MediaType.APPLICATION_JSON));
+
+        CommentDto dto = new CommentDto();
+        dto.setText("Great!");
+        ResponseEntity<Object> response = client.addComment(1L, 10L, dto);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+}
---
+++ gateway/src/test/java/ru/practicum/shareit/request/ItemRequestClientTest.java
@@ -0,0 +1,78 @@
+package ru.practicum.shareit.request;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.client.MockRestServiceServer;
+import ru.practicum.shareit.request.dto.NewItemRequestDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+class ItemRequestClientTest {
+
+    private ItemRequestClient client;
+    private MockRestServiceServer server;
+
+    @BeforeEach
+    void setUp() {
+        client = new ItemRequestClient("http://localhost:9090", new RestTemplateBuilder());
+        server = MockRestServiceServer.bindTo(client.getRestTemplate()).build();
+    }
+
+    @Test
+    void createRequest_SendsPostToRequestsEndpoint() {
+        server.expect(requestTo("http://localhost:9090/requests"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess("{\"id\":1}", MediaType.APPLICATION_JSON));
+
+        NewItemRequestDto dto = new NewItemRequestDto();
+        dto.setDescription("Need a drill");
+        ResponseEntity<Object> response = client.createRequest(1L, dto);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void getOwnRequests_SendsGetToRequestsEndpoint() {
+        server.expect(requestTo("http://localhost:9090/requests"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.getOwnRequests(1L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void getAllRequests_SendsGetToAllEndpoint() {
+        server.expect(requestTo("http://localhost:9090/requests/all"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.getAllRequests(1L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+
+    @Test
+    void getRequestById_SendsGetToRequestsIdEndpoint() {
+        server.expect(requestTo("http://localhost:9090/requests/5"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("{\"id\":5}", MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> response = client.getRequestById(1L, 5L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        server.verify();
+    }
+}
